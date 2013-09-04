891ed54ea5f1bf39b04d936cd424d44788530b6a
DmitryXann <dima@e-design.org.ua>
2013-09-05 01:15:22 +0300

    custom exceptions + gitignore

---
+++ WorkTimeAutomaticControlWPF/Exceptions/CloudWorkerException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace WorkTimeAutomaticControl.Exceptions
+{
+	/// <summary>
+	/// Exception for cloud worker
+	/// </summary>
+	internal class CloudWorkerException : Exception
+	{
+		internal CloudWorkerException(string message, Exception innerException)
+			: base(message, innerException)
+		{ }
+
+		internal CloudWorkerException(string message)
+			: base(message)
+		{ }
+	}
+}
---
+++ WorkTimeAutomaticControlWPF/Exceptions/RegystryWorkException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WorkTimeAutomaticControl.Exceptions
+{
+	/// <summary>
+	/// Exception for registry worker
+	/// </summary>
+	internal class RegystryWorkException : Exception
+	{
+		internal RegystryWorkException(string message)
+			: base(message)
+		{ }
+	}
+}
